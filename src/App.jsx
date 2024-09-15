a541bf922daca6d0525452696ff53e51c71dd0d0
90saifali11 <154356430+90saifali11@users.noreply.github.com>
2024-09-15 22:48:22 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Products from './pages/Products';
-import About from './pages/About';
 import Home from "./pages/Home";
 import ProductDetail from "./pages/ProductDetail"; // Ensure the file extension matches
-import SigninForm from './pages/Signin'; // Capitalized filename to match import convention
-import UserProfile from './pages/Profile'; // Capitalized filename to match import convention
-import Cart from './pages/cart';
 import SignupForm from './pages/Signup';
+import SigninForm from './pages/Signin';
+import Cart from './pages/cart';
+import UserProfile from './pages/Profile';
+import About from './pages/About';
 
 function App() {
   return (
@@ -25,6 +25,7 @@ function App() {
         <Route path="/signup" element={<SignupForm />} />
         <Route path="*" element={<h1>Page Not Found</h1>} />
       </Routes>
+      <Products/>
     </BrowserRouter>
   );
 }
